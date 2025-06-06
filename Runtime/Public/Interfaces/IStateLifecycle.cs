e9b37f0506255e1e21193cd3d5a4e4f701adefc1
Stefano Pittalis <stefano.pittalis @hotmail.it>
2025-06-06 11:22:47 +0200

    Split IStatechart in multiple interfaces

---
+++ Runtime/Public/Interfaces/IStateLifecycle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maze.StateFoundry
+{
+    public interface IStateLifecycle
+    {
+        public void OnEnter<TState>(Action<TState> callback) where TState : State, new();
+        public void OnExit<TState>(Action<TState> callback) where TState : State, new();
+        public void OnCreate<TState>(Action<TState> callback) where TState : State, new();
+        public void OnDispose<TState>(Action<TState> callback) where TState : State, new();
+    }
+}
