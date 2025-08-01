59e50174625ca40d294e10be7c673a97e5627591
Stefano Pittalis <stefano.pittalis@hotmail.it>
2025-08-02 00:12:11 +0200

    Add StateFactory tests
    - Introduced unit tests for the StateFactory class to ensure correct
    instance creation.
    - Validates behavior with valid state types, null input, invalid types,
    and abstract states.
    - Prevents regressions by asserting exceptions are thrown for improper
    inputs.
    - Improves code reliability and maintainability by covering critical
    factory scenarios.

---
+++ Tests/Runtime/Creation/StateFactory/StateFactoryTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Maze.StateFoundry.Tests
+{
+    [TestFixture]
+    public class StateFactoryTests
+    {
+        IStateFactory m_factory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_factory = new StateFactory();
+        }
+
+        [Test]
+        public void Given_ValidStateType_When_GetInstance_Then_ReturnsInstance()
+        {
+            IInternalState instance = m_factory.Build(typeof(DummyState));
+            Assert.IsNotNull(instance);
+            Assert.IsInstanceOf<DummyState>(instance);
+        }
+
+        [Test]
+        public void Given_NullType_When_GetInstance_Then_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => m_factory.Build(null));
+        }
+
+        [Test]
+        public void Given_TypeNotAssignableToState_When_GetInstance_Then_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => m_factory.Build(typeof(string)));
+        }
+
+        [Test]
+        public void Given_AbstractStateType_When_GetInstance_Then_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => m_factory.Build(typeof(AbstractDummyState)));
+        }
+
+        class DummyState : State { }
+        abstract class AbstractDummyState : State { }
+    }
+}
